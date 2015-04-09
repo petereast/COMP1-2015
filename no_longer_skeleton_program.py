67c6ea97830f0ad50e87b583cd94b885fe39eb8e
Peter East <08peast@gmail.com>
2015-04-09 17:02:02 +0100

    Added a few well required comments

--- no_longer_skeleton_program.py
+++ no_longer_skeleton_program.py
@@ -3,6 +3,16 @@
 # written by the AQA COMP1 Programmer Team
 # developed in the Python 3.4 programming environment
 
+
+
+###How I do my stuff:
+
+### One hash for Pseudocode
+
+### Two for comments
+
+### three for anything else
+
 BOARDDIMENSION = 8
 
 def CreateBoard():
@@ -22,9 +32,9 @@ def DisplayWhoseTurnItIs(WhoseTurn):
 def GetTypeOfGame():
   choice = ''
   while choice not in ['yes', 'no', 'y', 'n']:
-    choice = input("Do you want to play the sample game (enter Y for Yes)? ").lower()    
-  #return the stuff for the thing.
-  #the first character of the choice will be what the program is expecting, and from what I can make out it is also in uppercase.
+    choice = input("Do you want to play the sample game (enter Y for Yes)? ").lower()
+
+  ## the first character of the choice will be what the program is expecting, and from what I can make out it is also in uppercase.
   TypeOfGame = choice[0].upper()
   return TypeOfGame
 
@@ -33,9 +43,13 @@ def DisplayWinner(WhoseTurn):
     print("Black's Sarrum has been captured.  White wins!")
   else:
     print("White's Sarrum has been captured.  Black wins!")
-
+ 
 def CheckIfGameWillBeWon(Board, FinishRank, FinishFile):
-  if Board[FinishRank][FinishFile][1] == "S":
+  
+  ## this function will go and check if there is a sarrum in the space
+  ## that is going to be moved into
+  
+  if Board[FinishRank][FinishFile][1] == "S": ## if the peice at the finishing position is a sarrum
     return True
   else:
     return False
@@ -55,77 +69,107 @@ def DisplayBoard(Board):
   print()    
 
 def CheckRedumMoveIsLegal(Board, StartRank, StartFile, FinishRank, FinishFile, ColourOfPiece):
+  
   CheckRedumMoveIsLegal = False
+  
   if ColourOfPiece == "W":
     if FinishRank == StartRank - 1:
       if FinishFile == StartFile and Board[FinishRank][FinishFile] == "  ":
         CheckRedumMoveIsLegal = True
       elif abs(FinishFile - StartFile) == 1 and Board[FinishRank][FinishFile][0] == "B":
         CheckRedumMoveIsLegal = True
+        
   elif FinishRank == StartRank + 1:
     if FinishFile == StartFile and Board[FinishRank][FinishFile] == "  ":
       CheckRedumMoveIsLegal = True
     elif abs(FinishFile - StartFile) == 1 and Board[FinishRank][FinishFile][0] == "W":
       CheckRedumMoveIsLegal = True
+
+      
   return CheckRedumMoveIsLegal
 
 def CheckSarrumMoveIsLegal(Board, StartRank, StartFile, FinishRank, FinishFile):
+  
   CheckSarrumMoveIsLegal = False
-  if abs(FinishFile - StartFile) <= 1 and abs(FinishRank - StartRank) <= 1:
+  
+  if abs(FinishFile - StartFile) <= 1 and abs(FinishRank - StartRank) <= 1: ## this means the sarrum doesn' have to move
     CheckSarrumMoveIsLegal = True
+    
   return CheckSarrumMoveIsLegal
 
 def CheckGisgigirMoveIsLegal(Board, StartRank, StartFile, FinishRank, FinishFile):
+  
   GisgigirMoveIsLegal = False
   RankDifference = FinishRank - StartRank
   FileDifference = FinishFile - StartFile
+  
   if RankDifference == 0:
+    ## rank difference of zero means horizontal movement
+    ## if the peice is moving to the left
     if FileDifference >= 1:
       GisgigirMoveIsLegal = True
+      ## check that there are no peices in between first and final places.
       for Count in range(1, FileDifference):
         if Board[StartRank][StartFile + Count] != "  ":
           GisgigirMoveIsLegal = False
+    ## if the peice is moving to the right
     elif FileDifference <= -1:
       GisgigirMoveIsLegal = True
+      ## check that there are no peices in between
       for Count in range(-1, FileDifference, -1):
         if Board[StartRank][StartFile + Count] != "  ":
           GisgigirMoveIsLegal = False
+          
   elif FileDifference == 0:
+    ## file difference of zero means vertical
+    ## if the peice is moving up
     if RankDifference >= 1:
       GisgigirMoveIsLegal = True
+      ## check that all the spaces between it and it's final space
       for Count in range(1, RankDifference):
         if Board[StartRank + Count][StartFile] != "  ":
           GisgigirMoveIsLegal = False
+    ## if the object is moving down
     elif RankDifference <= -1:
       GisgigirMoveIsLegal = True
+      ## check that all the spaces in between it and it's final destination are empty
       for Count in range(-1, RankDifference, -1):
         if Board[StartRank + Count][StartFile] != "  ":
           GisgigirMoveIsLegal = False
-  return GisgigirMoveIsLegal
+  ## return the bool, true if legal, false if illegal
+  return GisgigirMoveIsLegal #bool
 
 def CheckNabuMoveIsLegal(Board, StartRank, StartFile, FinishRank, FinishFile):
   CheckNabuMoveIsLegal = False
+  ## check that the nabu moves only one space diagonally
   if abs(FinishFile - StartFile) == 1 and abs(FinishRank - StartRank) == 1:
     CheckNabuMoveIsLegal = True
-  return CheckNabuMoveIsLegal
+  return CheckNabuMoveIsLegal #bool
 
 def CheckMarzazPaniMoveIsLegal(Board, StartRank, StartFile, FinishRank, FinishFile):
   CheckMarzazPaniMoveIsLegal = False
+  ## can move either vertically or horizontally
   if (abs(FinishFile - StartFile) == 1 and abs(FinishRank - StartRank) == 0) or (abs(FinishFile - StartFile) == 0 and abs(FinishRank - StartRank) ==1):
     CheckMarzazPaniMoveIsLegal = True
   return CheckMarzazPaniMoveIsLegal
 
 def CheckEtluMoveIsLegal(Board, StartRank, StartFile, FinishRank, FinishFile):
   CheckEtluMoveIsLegal = False
+  ## can move exactly 2 in any direction
+  ## does not take into account the fact it cannot jump spaces
   if (abs(FinishFile - StartFile) == 2 and abs(FinishRank - StartRank) == 0) or (abs(FinishFile - StartFile) == 0 and abs(FinishRank - StartRank) == 2):
     CheckEtluMoveIsLegal = True
   return CheckEtluMoveIsLegal
 
 def CheckMoveIsLegal(Board, StartRank, StartFile, FinishRank, FinishFile, WhoseTurn):
   MoveIsLegal = True
+  ##if there is no movement then the move is not valid
   if (FinishFile == StartFile) and (FinishRank == StartRank):
     MoveIsLegal = False
+  ##movement helps in making the move valid
   else:
+
+    ## get the piece data from the arraay
     PieceType = Board[StartRank][StartFile][1]
     PieceColour = Board[StartRank][StartFile][0]
     if WhoseTurn == "W":
@@ -167,27 +211,38 @@ def InitialiseBoard(Board, SampleGame):
     Board[3][8] = "BE"
     Board[6][8] = "BR"
   else:
+    ##this bit sets up the board for a normal game, with all the peices in
+    ##their proper place.
     for RankNo in range(1, BOARDDIMENSION + 1):
       for FileNo in range(1, BOARDDIMENSION + 1):
         if RankNo == 2:
           Board[RankNo][FileNo] = "BR"
+          
         elif RankNo == 7:
           Board[RankNo][FileNo] = "WR"
+          
         elif RankNo == 1 or RankNo == 8:
           if RankNo == 1:
             Board[RankNo][FileNo] = "B"
+            
           if RankNo == 8:
             Board[RankNo][FileNo] = "W"
+            
           if FileNo == 1 or FileNo == 8:
             Board[RankNo][FileNo] = Board[RankNo][FileNo] + "G"
+            
           elif FileNo == 2 or FileNo == 7:
             Board[RankNo][FileNo] = Board[RankNo][FileNo] + "E"
+            
           elif FileNo == 3 or FileNo == 6:
             Board[RankNo][FileNo] = Board[RankNo][FileNo] + "N"
+            
           elif FileNo == 4:
             Board[RankNo][FileNo] = Board[RankNo][FileNo] + "M"
+            
           elif FileNo == 5:
             Board[RankNo][FileNo] = Board[RankNo][FileNo] + "S"
+            
         else:
           Board[RankNo][FileNo] = "  "    
                     
